55d1d0f6e70dfed8eca31e5e39d5142ee8c07431
ajay-0400mishra <ajaymishra1881@gmail.com>
2025-07-03 20:24:57 +0400

    Update app.py

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def describe_plot(fig, caption):
 def encode_label(series):
     mapping = {"Yes":1, "Maybe":0, "No":0}
     return series.map(mapping).fillna(0).astype(int)
-))
+)
 
 # --------- Data Visualization Tab ---------
 if tabs == "Data Visualization":
